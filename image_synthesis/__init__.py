df0a15963ca8ae52ae10ee243e64d23ae410f744
Cyril <cyrilcsy@gmail.com>
2022-10-17 13:34:39 +0800

    sat

--- image_synthesis/__init__.py
+++
@@ -1,2 +0,0 @@
-from image_synthesis.utils.io import *
-from image_synthesis.utils.misc import *
--- scripts/inference.py
+++ scripts/inference.py
@@ -17,8 +17,8 @@ from PIL import Image
 from collections import OrderedDict, defaultdict
 from torch.utils.data import ConcatDataset, dataset
 
-from image_synthesis import load_yaml_config, load_dict_from_json, save_dict_to_json
-from image_synthesis import get_all_file, get_all_subdir, instantiate_from_config
+from image_synthesis.utils.io import load_yaml_config, load_dict_from_json, save_dict_to_json
+from image_synthesis.utils.misc import get_all_file, get_all_subdir, instantiate_from_config
 from image_synthesis.utils.cal_metrics import get_PSNR, get_mse_loss, get_l1_loss, get_SSIM, get_mae
 from image_synthesis.modeling.build import build_model
 from image_synthesis.utils.misc import format_seconds, merge_opts_to_config
