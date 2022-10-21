e4ef91041c0cd51ce093f13c7c84c810d372d75c
Cyril <cyrilcsy@gmail.com>
2022-10-21 18:30:30 +0800

    sat

--- image_synthesis/modeling/codecs/image_codec/svq_gan.py
+++ image_synthesis/modeling/codecs/image_codec/svq_gan.py
@@ -356,7 +356,7 @@ class VectorQuantizer(nn.Module):
             sem_cls = self.semantic_classifier(self.embedding[:self.n_cluster])
             sem_cls = sem_cls / torch.norm(sem_cls, p=2, dim=-1, keepdim=True)
             b = int(z.shape[0] / sem_cls.shape[-1])
-            sem_cls = sem_cls.expand(-1, -1, b).view(self.n_cluster, -1).permute(1, 0)
+            sem_cls = sem_cls.expand(sem_cls[0], sem_cls[1], b).view(self.n_cluster, -1).permute(1, 0)
             d_from_center = self.alpha * sem_cls - (1 - self.alpha) * d_from_center.detach()  # L x k
             # method 1
             # token_semantic_type = torch.argmax(d_from_center, dim=1)
